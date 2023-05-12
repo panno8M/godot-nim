c9db8e9eb096f343f21a216c57d2d91a631d10f2
la .panon <panno.800k@gmail.com>
2023-05-13 02:03:07 +0900

    remove unused file

--- src/godot/builtinClasses.nim
+++
@@ -1,5 +0,0 @@
-## This module is generated automatically.
-import builtinClasses/gen; export gen
-import builtinClasses/opt; export opt
-import builtinClasses/essentials; export essentials
-import builtinClasses/constructors; export constructors
