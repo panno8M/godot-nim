9288255b6bd28ed0438ba809404c6e22ca30dd63
la .panon <panno.800k@gmail.com>
2023-09-09 03:01:12 +0900

    fix variant type resolving

--- src/godot/helper/variantTypeSolver.nim
+++ src/godot/helper/variantTypeSolver.nim
@@ -4,14 +4,15 @@ import ../godotInterface/objectBase
 import std/macros
 
 macro variantType*(Type: typedesc[SomeVariants]): Variant_Type =
-  ident "VariantType" & $Type.getTypeInst[1]
+  result = ident "VariantType" & $Type.getTypeInst[1]
 
-template variantType*(Type: typedesc): Variant_Type =
-  VariantType_Nil
 template variantType*(Type: typedesc[ObjectPtr]): Variant_Type =
   VariantType_Object
 template variantType*(Type: typedesc[SomeClass]): Variant_Type =
   VariantType_Object
+
+template variantType*(Type: typedesc[ptr Variant | Variant]): Variant_Type =
+  VariantType_Nil
 
 template variantType*(Type: typedesc[int32|int16|int8|uint64|uint32|uint16|uint8|byte]): Variant_Type =
   VariantType_Int
