17e034957dd39427535a34fd75daef95c5057dc5
la .panon. <panno.800k@gmail.com>
2023-09-22 03:05:59 +0900

    Change the function registration method.

--- demo/src/godotSideTester.nim
+++ demo/src/godotSideTester.nim
@@ -1,29 +1,26 @@
 import godot
 
 type GodotSideTester* = ref object of Node
-  int_value: int
-  float_value: float
+  int_value_raw: int
+  float_value_raw: float
 define_godot_class_essencials GodotSideTester, Node
 
-proc helloworld*(self: GodotSideTester): string =
+# If do not specify the method name on `exportgd`, it uses Nim's one instead.
+# In this case, it will be "helloworld."
+proc helloworld*(self: GodotSideTester): string {.exportgd.} =
   "Hello, World!"
-proc set_int_value*(self: GodotSideTester; value: int) =
-  self.int_value = value
-proc get_int_value*(self: GodotSideTester): int =
-  self.int_value
-proc set_float_value*(self: GodotSideTester; value: float) =
-  self.float_value = value
-proc get_float_value*(self: GodotSideTester): float =
-  self.float_value
+proc `int_value=`*(self: GodotSideTester; value: int) {.exportgd: "set_int_value".} =
+  self.int_value_raw = value
+proc int_value*(self: GodotSideTester): int {.exportgd: "get_int_value".} =
+  self.int_value_raw
+proc set_float_value*(self: GodotSideTester; value: float) {.exportgd.} =
+  self.float_value_raw = value
+proc get_float_value*(self: GodotSideTester): float {.exportgd.} =
+  self.float_value_raw
 
 # fold into macro in future
 # FIXME Currently, name conflicts can occur.
 proc register*(T: typedesc[GodotSideTester]) =
   register_class(GodotSideTester)
-  GodotSideTester.register_method(helloworld)
-  GodotSideTester.register_method(set_int_value)
-  GodotSideTester.register_method(get_int_value)
   GodotSideTester.add_property(int, "int_value", "set_int_value", "get_int_value")
-  GodotSideTester.register_method(set_float_value)
-  GodotSideTester.register_method(get_float_value)
   GodotSideTester.add_property(float, "float_value", "set_float_value", "get_float_value")
--- src/godot/helper/classDefiner.nim
+++ src/godot/helper/classDefiner.nim
@@ -13,25 +13,21 @@ when TraceEngineAllocationCallback:
   import ../logging
   template me: GDLogData = iam("allocation-hook", stgLibrary)
 
-type
-  ClassRegistrationInfo* = object
-    name*, parent_name*: StringName
-    creationInfo*: ClassCreationInfo
+type RegistrationData = ref object
+  methods*: seq[proc()]
+var registrationTable: Table[string, RegistrationData]
+proc get_registrationData*(T: typedesc[SomeUserClass]): RegistrationData =
+  if unlikely(not registrationTable.hasKey($T)):
+    registrationTable[$T] = new RegistrationData
+  registrationTable[$T]
 
-proc register_class(info: ClassRegistrationInfo) =
-  interfaceClassdbRegisterExtensionClass(library, addr info.name, addr info.parent_name, addr info.creationInfo)
 template register_class*(T: typedesc[SomeUserClass]) =
-  mixin make_ClassRegistrationInfo
   mixin bind_virtuals
-  register_class(T.make_ClassRegistrationInfo(false, false))
+  mixin initialize_class
+  let info = T.creationInfo(false, false)
+  interfaceClassdbRegisterExtensionClass(library, addr className(T), addr className(Inherit(T)), addr info)
   EngineClass(T).bind_virtuals(T)
-
-template register_method*(T: typedesc[SomeUserClass]; info: ClassMethodInfo) =
-  interface_classDbRegisterExtensionClassMethod(library, addr className(T), addr info)
-template register_method*(T: typedesc[SomeUserClass]; p: proc) =
-  mixin build_methodInfo
-  let info = build_methodInfo(p)
-  T.register_method(info)
+  for p in get_registrationData(T).methods: p()
 
 proc add_property*(T: typedesc[SomeUserClass]; info: PropertyInfo; setter, getter: static string) =
   let setter: StringName = setter
@@ -56,39 +52,35 @@ template define_godot_class_essencials*(Class, Inherits: typedesc): untyped =
     GC_ref new_object
     return new_object.owner
 
-  proc make_ClassRegistrationInfo*(T: typedesc[Class]; is_virtual, is_abstract: bool): ClassRegistrationInfo =
-    bind className
-
-    ClassRegistrationInfo(
-      name: className(T),
-      parent_name: className(Inherit(T)),
-      creationInfo: ClassCreationInfo(
-        is_virtual: is_virtual,
-        is_abstract: is_abstract,
-        set_func: set_bind,
-        get_func: get_bind,
-        get_property_list_func: get_property_list_bind,
-        free_property_list_func: free_property_list_bind,
-        property_can_revert_func: property_can_revert_bind,
-        property_get_revert_func: property_get_revert_bind,
-        notification_func: notification_bind,
-        to_string_func: to_string_bind,
-        create_instance_func: create,
-        free_instance_func: free_bind,
-        get_virtual_func: getVirtual,
-        class_userdata: get_userdata(T),
-      )
+  proc creationInfo*(T: typedesc[Class]; is_virtual, is_abstract: bool): ClassCreationInfo =
+    ClassCreationInfo(
+      is_virtual: is_virtual,
+      is_abstract: is_abstract,
+      set_func: set_bind,
+      get_func: get_bind,
+      get_property_list_func: get_property_list_bind,
+      free_property_list_func: free_property_list_bind,
+      property_can_revert_func: property_can_revert_bind,
+      property_get_revert_func: property_get_revert_bind,
+      notification_func: notification_bind,
+      to_string_func: to_string_bind,
+      create_instance_func: create,
+      free_instance_func: free_bind,
+      get_virtual_func: getVirtual,
+      class_userdata: get_userdata(T),
     )
 
-macro build_methodInfo*(Proc: proc): ClassMethodInfo =
-  let ProcDef = Proc.getImpl
-  let params = ProcDef.params
+type ClassMethodInfoGlue = ref object
+  info*: ClassMethodInfo
+proc build_methodInfo_fromdef(procdef: NimNode; gdname: NimNode): NimNode =
+  let params = procDef.params
   let ret_t = params[0]
   let self_def = params[1]
   let self_t = self_def[1]
-  let proc_name = toStrLit Proc
-  let has_return = ProcDef.hasReturn
+  let has_return = procdef.hasReturn
   var argcount: int
+  var proc_name = procdef[0]
+  if proc_name.kind == nnkPostfix: proc_name = proc_name[1]
 
   let return_value_info =
     if has_return:
@@ -106,7 +98,7 @@ macro build_methodInfo*(Proc: proc): ClassMethodInfo =
     var info = newNimNode nnkBracket
     var meta = newNimNode nnkBracket
     for i, (name, Type, default) in params.breakArgs:
-      if i == 0: continue
+      if i == 0: continue # [0: self/typedesc[self], 1: arg1, 2: arg2...]
       let name = toStrLit name
       inc argcount
       info.add quote do:
@@ -123,44 +115,39 @@ macro build_methodInfo*(Proc: proc): ClassMethodInfo =
 
   var call_func_body = newStmtList()
   var ptrcall_func_body = newStmtList()
-  let r_return = genSym(nskParam, "r_return")
-  let r_ret = genSym(nskParam, "r_ret")
-  let p_instance_call = genSym(nskParam, "p_instance")
-  let p_instance_ptrcall = genSym(nskParam, "p_instance")
-  let p_args_call = genSym(nskParam, "p_args")
-  let p_args_ptrcall = genSym(nskParam, "p_args")
   block:
-    var call = Proc.newCall()
-    var ptrcall = Proc.newCall()
-    call.add quote do: cast[`self_t`](`p_instance_call`)
-    ptrcall.add quote do: cast[`self_t`](`p_instance_ptrcall`)
+    var call = proc_name.newCall()
+    var ptrcall = proc_name.newCall()
+    call.add quote do: cast[`self_t`](p_instance)
+    ptrcall.add quote do: cast[`self_t`](p_instance)
 
     for i, (name, Type, default) in params.breakArgs:
-      if i == 0: continue
+      if i == 0: continue # [0: self/typedesc[self], 1: arg1, 2: arg2...]
       let i_lit = newlit i - 1
-      call.add quote do: `p_args_call`[`i_lit`].get(typedesc `Type`)
-      ptrcall.add quote do: `p_args_ptrcall`[`i_lit`].decode(typedesc `Type`)
+      call.add quote do: p_args[`i_lit`].get(typedesc `Type`)
+      ptrcall.add quote do: p_args[`i_lit`].decode(typedesc `Type`)
     if has_return:
       call_func_body = quote do:
         let result = variant `call`
-        interface_Variant_newCopy(`r_return`, addr result)
+        interface_Variant_newCopy(r_return, addr result)
       ptrcall_func_body = quote do:
-        `ptrcall`.encode(`r_ret`)
+        `ptrcall`.encode(r_ret)
     else:
       call_func_body = quote do:
         let result = variant(); `call`
-        interface_Variant_newCopy(`r_return`, addr result)
+        interface_Variant_newCopy(r_return, addr result)
       ptrcall_func_body = ptrcall
 
   let argcount_lit = newlit argcount
   let hasReturn_lit = newlit hasReturn
+
   result = quote do:
-    proc call_func(method_userdata: pointer; `p_instance_call`: ClassInstancePtr; `p_args_call`: UncheckedArray[ConstVariantPtr]; p_argument_count: Int; `r_return`: VariantPtr; r_error: ptr CallError) {.gdcall, gensym.} =
+    proc call_func(method_userdata: pointer; p_instance {.inject.}: ClassInstancePtr; p_args {.inject.}: UncheckedArray[ConstVariantPtr]; p_argument_count: Int; r_return {.inject.}: VariantPtr; r_error: ptr CallError) {.gdcall, gensym.} =
       `call_func_body`
-    proc ptrcall_func(method_userdata: pointer; `p_instance_ptrcall`: ClassInstancePtr; `p_args_ptrcall`: UncheckedArray[ConstTypePtr]; `r_ret`: TypePtr) {.gdcall, gensym.} =
+    proc ptrcall_func(method_userdata: pointer; p_instance {.inject.}: ClassInstancePtr; p_args {.inject.}: UncheckedArray[ConstTypePtr]; r_ret {.inject.}: TypePtr) {.gdcall, gensym.} =
       `ptrcall_func_body`
 
-    let proc_name: StringName = `proc_name`
+    let proc_name: StringName = `gdname`
 
     # const default_argument_count: uint32 = 1
     # let default_arguments: array[default_argument_count, Variant] = [
@@ -169,21 +156,45 @@ macro build_methodInfo*(Proc: proc): ClassMethodInfo =
     # let p_default_arguments: array[default_argument_count, ptr Variant] = [
     #   addr default_arguments[0]
     # ]
+    let res = new ClassMethodInfoGlue
 
-    ClassMethodInfo(
+    res.info = ClassMethodInfo(
       name: addr proc_name,
-        call_func: call_func,
-        ptrcall_func: ptrcall_func,
-        method_flags: {MethodFlag_Normal},
+      call_func: call_func,
+      ptrcall_func: ptrcall_func,
+      method_flags: {MethodFlag_Normal},
 
-        has_return_value: `has_return_lit`,
-        return_value_info: `return_value_info`,
-        return_value_metadata: `return_value_metadata`,
+      has_return_value: `has_return_lit`,
+      return_value_info: `return_value_info`,
+      return_value_metadata: `return_value_metadata`,
 
-        argument_count: `arg_count_lit`,
-        arguments_info: `arguments_info`,
-        arguments_metadata: `arguments_metadata`,
+      argument_count: `arg_count_lit`,
+      arguments_info: `arguments_info`,
+      arguments_metadata: `arguments_metadata`,
 
-        # default_argument_count: default_argument_count,
-        # default_arguments: addr p_default_arguments[0],
-      )
+      # default_argument_count: default_argument_count,
+      # default_arguments: addr p_default_arguments[0],
+    )
+    res
+
+proc exportgd_impl(body: NimNode; gdname: NimNode = nil): NimNode =
+  case body.kind
+  of nnkProcDef:
+    let arg0 = body.params[1][1]
+    let gdname = if gdname.isNil: body.name.toStrLit else: gdname
+    let methodinfoDef = build_methodInfo_fromdef(body, gdname)
+    result = newStmtList()
+    if body.name.kind != nnkSym:
+      result.add body
+    result.add quote do:
+      get_registrationData(typedesc `arg0`).methods.add proc() =
+        let glue = `methodinfoDef`
+        interface_classDbRegisterExtensionClassMethod(library, addr className(typedesc `arg0`), addr glue.info)
+    return
+  else:
+    warning $body.kind, body
+    return body
+macro exportgd*(gdname: string; body): untyped =
+  exportgd_impl(body, gdname)
+macro exportgd*(body: untyped): untyped =
+  result = exportgd_impl(body)
