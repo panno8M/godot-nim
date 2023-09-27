80aef44b7bd6db847509699d7ffaf356b1166d46
la .panon <panno.800k@gmail.com>
2023-09-27 23:56:45 +0900

    add vector swizzling

--- src/godot/pure/geometrics_swizzles.nim
+++ src/godot/pure/geometrics_swizzles.nim
@@ -1,5 +1,4 @@
 import std/macros
-import std/strutils
 import std/sequtils
 
 # Swizzle //================================================================== #
@@ -7,88 +6,45 @@ import std/sequtils
 proc subVec[N; T](v: var array[N,T]; offset, length: static[int]): var array[length,T] =
   cast[ptr array[length, T]](addr v[offset])[]
 
-proc head(n: NimNode): NimNode {.compileTime.} =
-  if n.kind == nnkStmtList and n.len == 1: result = n[0] else: result = n
-
 proc isContinuous(indices: varargs[int]): bool {.compileTime.} =
   for i in 1..indices.high:
     if indices[i-1] != indices[i]-1: return false
   return true
 
-proc isGrowing(indices: varargs[int]): bool {.compileTime.} =
-  ## returns true when every argument is bigger than all previous arguments
-  for i in 1..indices.high:
-    if indices[i-1] >= indices[i]: return false
-  return true
+proc keyMap(key: char): int =
+  case key
+  of 'x': 0
+  of 'y': 1
+  of 'z': 2
+  of 'w': 3
+  else: -1
 
-proc swizzleMethods(indices: varargs[int], chars: string): seq[NimNode] {.compileTime.}=
-  let name = indices.mapIt(chars[it]).join
-  let getter = ident(name)
-  let setter = ident(name & '=')
+macro swizzle*[I: static int; T](v: array[I, T]; key: untyped): untyped =
+  var indices: seq[int]
+  for k in $key:
+    let i = keyMap(k)
+    if i == -1: error "Invalid token. got " & k & ".", key
+    if i >= I: error "Out of range. got " & k & " and that will replace to " & $i & ".", key
+    indices.add i
 
   case indices.len
   of 0: discard
   of 1:
-    let lit = newLit(indices[0])
-    result.add quote do:
-      func `getter`*[N: static int; T](v: array[N,T]): T {.inline.} = v[`lit`]
-      func `getter`*[N: static int; T](v: var array[N,T]): var T {.inline.} = v[`lit`]
-      func `setter`*[N: static int; T](v: var array[N,T]; val: T) {.inline.} =
-        v[`lit`] = val
-
+    return nnkBracketExpr.newTree(v, newlit indices[0])
   else:
-    let Nlit = newLit(indices.len)
-
-    block:
-      let v = genSym(nskParam, "v")
-
-      let args = (@indices).map proc(it: int): NimNode =
-        let lit = newLit(it)
-        head quote do: `v`[`lit`]
-
-      let b = nnkBracket.newTree(args)
-
-      result.add quote do:
-        template `getter`*[N: static int; T](`v`: array[N,T]): auto =
-          `b`
-
     if indices.isContinuous:
-      let offsetLit = newLit(indices[0])
-      let lengthLit = newLit(indices.len)
-      result.add head quote do:
-        func `getter`*[N: static int; T](v: var array[N,T]): var array[`Nlit`,T] {.inline.} =
-          v.subVec(`offsetLit`, `lengthLit`)
+      return bindSym"subVec".newCall(v, newlit indices[0], newlit indices.len)
+    else:
+      let nv = genSym(nskLet, "nv")
+      return newStmtList(
+        nnkLetSection.newTree(nnkIdentDefs.newTree(nv, newEmptyNode(), v)),
+        nnkBracket.newTree indices.mapIt(nnkBracketExpr.newTree(nv, newlit it)),
+      )
 
-    if indices.isGrowing:
-      let N2lit = newLit(indices.len)
-      let v1 = genSym(nskParam, "v1")
-      let v2 = genSym(nskParam, "v2")
-
-      let assignments = newStmtList()
-      for i,idx in indices:
-        let litL = newLit(idx)
-        let litR = newLit(i)
-        assignments.add head quote do:
-          `v1`[`litL`] = `v2`[`litR`]
-
-      result.add head quote do:
-        proc `setter`*[N: static int; T](`v1`: var array[N,T]; `v2`: array[`N2lit`,T]) {.inline.} =
-          `assignments`
-
-macro genSwizzleOps(chars: static[string]): untyped =
-  result = newStmtList()
-  for i in 0 .. 3:
-    result.add swizzleMethods(i, chars)
-    for j in 0 .. 3:
-      result.add swizzleMethods(i,j, chars)
-      for k in 0 .. 3:
-        result.add swizzleMethods(i,j,k, chars)
-        for m in 0 .. 3:
-          result.add swizzleMethods(i,j,k,m, chars)
-
-{.push, inline.}
-genSwizzleOps("xyzw")
-{.pop.}
+template `{}`*[I: static int; T](v: array[I, T]; key: untyped): untyped =
+  v.swizzle(key)
+template `{}=`*[I: static int; T](v: array[I, T]; key: untyped; value): untyped =
+  v.swizzle(key) = value
 
 
 # ==================================================================// Swizzle #
@@ -98,41 +54,29 @@ when isMainModule:
   test "swizzling":
     var v = [1, 2, 3]
 
-    check not compiles v.d
-
     # echo v.x
-    check v.x == 1
-    check v.y == 2
-    check v.z == 3
-    check not compiles v.w
+    check v{x} == 1
+    check v{y} == 2
+    check v{z} == 3
+    check not compiles v{w}
+    check not compiles v{a}
 
-    check v.xy == [1, 2]
-    check v.yz == [2, 3]
-    check v.xz == [1, 3]
+    check v{yx} == [2, 1]
+    check v{yy} == [2, 2]
+    check v{yz} == [2, 3]
 
-    check v.zy == [3, 2]
-    check v.yx == [2, 1]
-    check v.zx == [3, 1]
+    check v{xy} == [1, 2]
+    check v{yy} == [2, 2]
+    check v{zy} == [3, 2]
 
-    check not compiles v.xw
-    check not compiles v.yw
-    check not compiles v.zw
+    check v{xxxxxxxxxx} == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-    check not compiles v.wx
-    check not compiles v.wy
-    check not compiles v.wz
+    v{xy} = [4, 5]
+    check v == [4, 5, 3]
 
-    check v.xyz == [1, 2, 3]
-    check v.xzy == [1, 3, 2]
-    check v.zxy == [3, 1, 2]
-    check v.zyx == [3, 2, 1]
-
-    check v.xxx == [1, 1, 1]
-    check v.yxy == [2, 1, 2]
-
-    var v4 = [1, 2, 3, 4]
-    check v4.w == 4
-
-  dumpTree:
-    x = y
-
+    var v2 = [1, 2, 3, 4]
+    check v2{x} == 1
+    check v2{y} == 2
+    check v2{z} == 3
+    check v2{w} == 4
+    check not compiles v2{a}
