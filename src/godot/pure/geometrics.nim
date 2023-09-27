39bd96070034143d2e5b9361c0f6f06e15aa239b
la .panon <panno.800k@gmail.com>
2023-09-28 00:54:05 +0900

    fix module dependencies

--- src/godot/pure/geometrics.nim
+++ src/godot/pure/geometrics.nim
@@ -1,7 +1,6 @@
 import geometrics_typedef;        export geometrics_typedef
+import geometrics_fmaps;          export geometrics_fmaps
 import geometrics_math;           export geometrics_math
 import geometrics_vectorCommons;  export geometrics_vectorCommons
 import geometrics_swizzles;       export geometrics_swizzles
 import geometrics_interpolations; export geometrics_interpolations
-
-import beyond/functional; export functional
